ab6318dc13e8d44d1cb589d745a4a5d3233ceeb8
Kiwi73 <tonyt73@gmail.com>
2024-12-15 10:20:49 +1000

    Added parameter support
    * Test name
    * Loops
    * Spawns
    Added a zip file in the test folder with all content used to re-run the test

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,35 +1,54 @@
+param (
+    $TestName = "Test x",
+    $Loops = 10,
+    $Spawns = 50
+)
+
 function RunTest {
     param (
+        $testName,
         $spawnCount
     )
+
+    Write-Host "Running: $testName, $spawnCount"
+
+    $path = "./Results/$TestName/"
+    $file = $path + "bcc-test.csv"
+
+    if ($spawnCount -eq 0) {
+        # reset the CSV file
+        New-Item -Path $path -ItemType Directory -Force -Confirm:$false -ErrorAction Ignore | Out-Null
+        "files, bcc32, bcc64, bcc64x" | Set-Content $file
+        # make a copy of the files used in the compile test, so we can recreate the test results if needed after the tests evolve over time
+        $zip = @{
+            Path = "bcc-test*.*", "unit*.*", "*.ps1", "*.bat"
+            CompressionLevel = "Fastest"
+            DestinationPath = $path + "files.zip"
+        }
+        Compress-Archive @zip -Force
+    }
 
     $bcc32time = Measure-Command { & .\build-bcc32.bat }
     $bcc64time = Measure-Command { & .\build-bcc64.bat }
     $bcc64xtime = Measure-Command { & .\build-bcc64x.bat }
 
-    if ($spawnCount -eq 0) {
-        # reset the CSV file
-        "files, bcc32, bcc64, bcc64x" | Set-Content "bcc-test.csv"
-    }
-
-    "$spawnCount, $($bcc32time.TotalSeconds), $($bcc64time.TotalSeconds), $($bcc64xtime.TotalSeconds)" | Add-Content "bcc-test.csv"
+    "$spawnCount, $($bcc32time.TotalSeconds), $($bcc64time.TotalSeconds), $($bcc64xtime.TotalSeconds)" | Add-Content $file
+    Write-Host "Completed: $testName, $spawnCount`r`n"
 }
 
 
 # restore the project
 Write-Host 
-.\restore.ps1
+.\Restore.ps1
 
-# loop through {$loops} times adding {$spawns} number of files each time
-$loops = 10
-$spawns = 50
 
 # time the original project
-RunTest 0
+RunTest $TestName 0
 
-for ($l = 1; $l -le $loops; $l++) {
+# loop through {$loops} times adding {$spawns} number of files each time
+for ($l = 1; $l -le $Loops; $l++) {
     # spawn the files
-    .\spawn.ps1 $spawns
+    .\Spawn.ps1 $Spawns
     # test the results
-    RunTest $($spawns * $l)
+    RunTest $TestName $($Spawns * $l)
 }
